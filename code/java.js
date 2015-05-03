a663e76951d279fdc477dbbe785284936525ea7d
ncisrobert <robert.stenhouse@students.plymouth.ac.uk>
2015-05-03 10:38:46 +0100

    First add
    add all files currently have

---
+++ code/java.js
@@ -0,0 +1,29 @@
+var divNames = ["Background", "charicter", "Movement", "Fall", "Fall2", "Extras", "UserName", "Submit"];
+
+function setup() {
+    alert("im working");
+     
+    for (i = 0; i <divNames.length; i++) { 
+        
+    document.getElementById(divNames[i]).style.display = "none";
+        
+    }
+}
+
+var counter = 0 ;
+
+function Next() {
+    
+    document.getElementById("Name").style.display ="none";
+    document.getElementById(divNames[counter]).style.display ="none";
+    counter = counter + 1;
+    document.getElementById(divNames[counter]).style.display ="block";
+        
+    if (counter == 6) {
+        
+         document.getElementById("Next").style.display ="none";
+          document.getElementById("Submit").style.display ="Block";
+    }
+    }
+    
+    
